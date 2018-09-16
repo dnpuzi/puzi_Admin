2a5ce68e4bbd6dc5201b455b17210857bf85da6a
tfciw <1315356284@qq.com>
2018-09-16 17:22:48 +0800

    feat:托管github

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dashboard-container">
+    dashboard
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboard'
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
